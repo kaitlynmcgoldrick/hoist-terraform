5cb9a7a0ba5f74c93941d18ce80cf7a7c2463e31
Kaitlyn McGoldrick <mcgoldrick.kaitlyn@gmail.com>
2020-08-10 07:45:02 -0700

    Host static site on s3 bucket

--- infrastructure.tf
+++ infrastructure.tf
@@ -2,3 +2,28 @@ provider "aws" {
   version = "~> 3.0"
   region  = "us-east-1"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "kaitlyn-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "Blog Bucket"
+    Environment = "Dev"
+  }
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
